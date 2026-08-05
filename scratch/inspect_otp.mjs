ecc31cca59be43997f31292f76abc67786fb3b82
ahmed000248 <ahmedraa0007@gmail.com>
2026-08-05 17:22:53 +0500

    chore: clean up temporary scratch test files

--- scratch/inspect_otp.mjs
+++
@@ -1,3 +0,0 @@
-import { createOTP } from "@better-auth/utils/otp";
-const otp = createOTP("BcPsLufdNG50sDr-u0kohfL7XrCrpKmv");
-console.log("createOTP keys:", Object.keys(otp), Object.getOwnPropertyNames(Object.getPrototypeOf(otp)));
